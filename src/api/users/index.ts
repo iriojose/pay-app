64803fa5dd7afee3655a663b746efbae79ce63e6
iriojose <iriojgomezv@gmail.com>
2024-11-07 23:39:16 -0400

    agregar servicios de llamadas a la api y ajustes en modelos

---
+++ src/api/users/index.ts
@@ -0,0 +1,33 @@
+
+import { User, AddBalance, CheckBalance } from "../../models/user";
+import { apiCommand } from "../apiFactory";
+
+const BASE_URL = import.meta.env.VITE_API_URL
+
+const getUsers = () => {
+    return apiCommand<User[]>('GET')(BASE_URL, 'users');
+}
+
+const getUser = (id: string) => {
+    return apiCommand<User>('GET')(BASE_URL, `users/${id}`)
+}
+
+const createUser = (data: Omit<User, 'createdAt' | 'updatedAt' | 'balance' | 'payments' | 'id'>) => {
+    return apiCommand<User>('POST')(BASE_URL, '/users/create', data)
+}
+
+const checkBalance = (data: CheckBalance) => {
+    return apiCommand<number>('POST')(BASE_URL, 'users/check-balance', data)
+}
+
+const addBalance = (data: AddBalance) => {
+    return apiCommand<number>('POST')(BASE_URL, '/users/add-balance', data)
+} 
+
+export {
+    getUsers,
+    getUser,
+    checkBalance,
+    addBalance,
+    createUser
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,17 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
+import { User } from "../../models/user"
+import { getUsers } from "../../api/users"
 
 export const Home: FC = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        getUsers().then(data => setUsers(data))
+    },[])
+
     return (
         <div>
-            home
+            {JSON.stringify(users)}
         </div>
     )
 } 
